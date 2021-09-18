c80264abe1d920068c4052476605b761385b1188
Charlie Sharpsteen <source@sharpsteen.net>
2021-09-18 12:02:23 -0500

    Add Bolt task to build puppet-agent
    This commit adds a `build_component` tasks to the Bolt project
    in `resources/bolt` that can build the `puppet-runtime` and
    `puppet-agent` packages on an ARM host. The Raspberry Pi OS
    workflow is updated to use this new task.
    
    This commit also adds patches required for native builds of
    puppet-agent 6.22.0 and newer.

---
+++ resources/bolt/tasks/build_component.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+declare PT_agent_version
+declare PT_project_version
+declare PT_component
+
+export VANAGON_USE_MIRRORS=n
+export VANAGON_DOCKER_IMAGE=ghcr.io/sharpie/raspbian-10-armhf
+
+if [[ ! -e puppet-build-experiment ]]; then
+  git clone --depth=1 https://github.com/Sharpie/puppet-build-experiment
+
+  pushd puppet-build-experiment
+
+  [[ -n "${PT_project_version}" ]] && git checkout "${PT_project_version}"
+
+  bundle config set path .bundle/lib
+  bundle install
+
+  popd
+fi
+
+pushd puppet-build-experiment
+
+case "${PT_component}" in
+  runtime)
+    runtime_version=$(./scripts/component-version.sh puppet-agent "{$PT_agent_version}" puppet-runtime)
+
+    env VANAGON_BUILD_VERSION="${runtime_version}" bundle exec build puppet-runtime debian-10-armhf -e docker
+    ;;
+  agent)
+    env VANAGON_BUILD_VERSION="${PT_agent_version}" bundle exec build puppet-agent debian-10-armhf -e docker
+    ;;
+esac
